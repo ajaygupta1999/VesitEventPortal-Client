d17b887c0869019b4ca341bac48500e895fd8af2
bighac <2018.ajay.gupta@ves.ac.in>
2020-12-18 15:54:24 +0530

    Initial repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+import Navbar from './components/navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      data : ""
+    }
+  }
+
+  async componentDidMount(){
+      console.log("doing request");
+      var resp = "";
+      let obj =  await axios.get("/google");
+      this.setState({
+        data : obj.data
+      });
+  }
+
+  render(){
+    return (
+      <div>
+          <Navbar />
+          <center><h1>
+            {
+               this.state.data === "" ? "Data is Loading.." : this.state.data
+            }
+          </h1>
+          </center>
+          <center><h1>Hey, This is vesit EventPortal</h1></center>
+      </div>
+    )
+  };
 }
 
 export default App;
